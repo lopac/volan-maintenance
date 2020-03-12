eb82c3e5235732c16e836680ae17a00deac7b91f
Antonio Lopac <antonio@lopac.co>
2020-03-12 18:18:27 +0100

    Updates

---
+++ precache-manifest.d58c20bea865060ee8861bb8cab4fbab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d5c422c32ce425a33cdcb1f2bd90b9d",
+    "url": "/volan-maintenance/index.html"
+  },
+  {
+    "revision": "0d6a9728b9d896978451",
+    "url": "/volan-maintenance/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "3990ed8cbbd6865a88d1",
+    "url": "/volan-maintenance/static/css/main.4f01667f.chunk.css"
+  },
+  {
+    "revision": "0d6a9728b9d896978451",
+    "url": "/volan-maintenance/static/js/2.7d59599f.chunk.js"
+  },
+  {
+    "revision": "8a14dcea7ad8b593c1e83c11f7ba1a89",
+    "url": "/volan-maintenance/static/js/2.7d59599f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3990ed8cbbd6865a88d1",
+    "url": "/volan-maintenance/static/js/main.d2c2ea11.chunk.js"
+  },
+  {
+    "revision": "d8148e9c602695fb906d",
+    "url": "/volan-maintenance/static/js/runtime-main.18163d63.js"
+  }
+]);
